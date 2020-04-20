59ace8900d6b3c45f9014d5f3cd03a9e63d5044c
Thomas10111 <fisch27@gmx.de>
2020-04-20 06:57:15 +0000

    model

---
+++ model.py
@@ -0,0 +1,19 @@
+from keras.layers import Cropping2D, Convolution2D, Dense, GlobalAveragePooling2D, Activation, Flatten, Lambda
+from keras.models import Sequential
+
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.save("model.h5")
